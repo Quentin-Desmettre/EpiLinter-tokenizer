6d60322bbe59468fbb186b43eeb6c8bb034706ab
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2023-01-26 14:26:55 +0100

    feat: removed debug prints

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@
 int main(int ac, char **av)
 {
     QCoreApplication app(ac, av);
-    Server serv(8081);
+    Server serv(8081, ac > 1 && av[1] == (std::string)("--debug"));
     return app.exec();
 }
